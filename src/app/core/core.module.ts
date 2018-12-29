a1340d623749c96622ab20fc4c7cde2de3d83c90
Rodrigo Coradi <wcrodrigo@hotmail.com>
2018-12-29 18:28:54 -0200

    conclusão do curso

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -17,12 +18,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     BrowserAnimationsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    RouterModule
   ],
   exports: [
       BrowserModule,
       BrowserAnimationsModule,
       HttpClientModule,
-      NavbarComponent
+      NavbarComponent,
+      RouterModule
   ]
 })
 export class CoreModule { }
--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,19 +1,16 @@
-import { Injectable, Injector } from '@angular/core';
-import { Observable } from 'rxjs';
-import { flatMap, catchError } from 'rxjs/operators';
-import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
-import { CategoryService } from './../../categories/shared/category.service';
-import { Entry } from './entry.model';
+import {Injectable, Injector} from '@angular/core';
+import {Observable} from 'rxjs';
+import {flatMap, catchError, map} from 'rxjs/operators';
+import {BaseResourceService} from 'src/app/shared/services/base-resource.service';
+import {CategoryService} from './../../categories/shared/category.service';
+import {Entry} from './entry.model';
+import * as moment from 'moment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class EntryService extends BaseResourceService<Entry> {
-
-    constructor(
-        private categoryService: CategoryService,
-        protected injector: Injector
-        ) {
+    constructor(private categoryService: CategoryService, protected injector: Injector) {
         super('api/entries', injector, Entry.fromJson);
     }
 
@@ -25,13 +22,33 @@ export class EntryService extends BaseResourceService<Entry> {
         return this.setCategoryAndSendToServer(entry, super.update.bind(this));
     }
 
+    getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+        return this.getAll().pipe(
+            map(entries => this.filterByMonthAndYear(entries, month, year))
+        );
+    }
+
     private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
         return this.categoryService.getById(entry.categoryId).pipe(
             flatMap(category => {
-              entry.category = category;
-              return sendFn(entry);
+                entry.category = category;
+                return sendFn(entry);
             }),
             catchError(this.handleError)
         );
+    }
+
+    private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+        return entries.filter(entry => {
+            const entryDate = moment(entry.date, 'DD/MM/YYYY');
+
+            // + 1 porque o month() inicia com 0 (janeiro)
+            const monthMatches = entryDate.month() + 1 == month;
+            const yearMatches = entryDate.year() == year;
+
+            if (monthMatches && yearMatches) {
+                return entry;
+            }
+        });
     }
 }
